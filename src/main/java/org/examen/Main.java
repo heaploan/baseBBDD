e25d78c40afdff83e25d6131f2afce5931df8f64
Apolo <kesh.andrade@gmail.com>
2024-05-22 14:50:13 +0200

    Main updated

--- src/main/java/org/examen/Main.java
+++ src/main/java/org/examen/Main.java
@@ -1,8 +1,10 @@
 package org.examen;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.examen.view.Menu;
+
 public class Main {
     public static void main(String[] args) {
-
+        Menu menu = new Menu();
+        menu.menu();
     }
+}
