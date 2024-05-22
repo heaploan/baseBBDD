9c3ef26fe688c7966f97c8e609fe87a61d81cff8
Apolo <kesh.andrade@gmail.com>
2024-05-22 15:14:54 +0200

    all updated

--- src/main/java/org/examen/model/Objeto1.java
+++ src/main/java/org/examen/model/Objeto1.java
@@ -1,7 +1,37 @@
 package org.examen.model;
 
 public class Objeto1 {
+    private String nombre;
+    private String loQueSea;
+    private String loQueSea2;
 
     public Objeto1(String nombre, String loQueSea, String loQueSea2) {
+        this.nombre = nombre;
+        this.loQueSea = loQueSea;
+        this.loQueSea2 = loQueSea2;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getLoQueSea() {
+        return loQueSea;
+    }
+
+    public String getLoQueSea2() {
+        return loQueSea2;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setLoQueSea(String loQueSea) {
+        this.loQueSea = loQueSea;
+    }
+
+    public void setLoQueSea2(String loQueSea2) {
+        this.loQueSea2 = loQueSea2;
     }
 }
